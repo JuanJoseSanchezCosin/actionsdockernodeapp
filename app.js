9b96787aa57ef46a6724a7795878df694b1f508b
cousine <cousine@vbox.myguest.virtualbox.org>
2026-02-12 11:44:13 +0100

    Añadida ruta /api para tests

--- app.js
+++ app.js
@@ -16,31 +16,31 @@ var animals = {
 }
 
 function getAnimal() {
-  return animal = _.sample(Object.entries(animals));
+    return animal = _.sample(Object.entries(animals));
 }
 
 const app = express();
 
 app.get('/', function(req, res){
-  const [animal_name, sound] = getAnimal();
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`George Orwell had a farm.<br />
+    const [animal_name, sound] = getAnimal();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`George Orwell had a farm.<br />
 E-I-E-I-O<br />
-And on his farm he had a ${ animal_name }.<br />
+And on his farm he had a ${animal_name}.<br />
 E-I-E-I-O<br />
-With a ${ sound }-${ sound } here.<br />
-And a ${ sound }-${ sound } there.<br />
-Here a ${ sound }, there a ${ sound }.<br />
-Everywhere a ${ sound }-${ sound }.<br />`);
-      res.end();
+With a ${sound}-${sound} here.<br />
+And a ${sound}-${sound} there.<br />
+Here a ${sound}, there a ${sound}.<br />
+Everywhere a ${sound}-${sound}.<br />
+`);
+    res.end();
 });
 
-app.get('/api', function(req, res){
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify(animals));
-  res.end();
-})
 
-module.exports =  app.listen(port, () => {
-  console.log(`Launching server on http://localhost:${ port }`)
+app.get('/api', function(req, res){
+    res.json(animals);
+});
+
+app.listen(port, function(){
+    console.log(`Launching server on http://localhost:${port}`);
 });
